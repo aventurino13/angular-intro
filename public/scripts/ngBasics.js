a52e06fffa03761fffeb9d6e3662fb764dc69b12
Amy Venturino <aventuri@umich.edu>
2017-05-09 17:13:22 -0500

    refactoring array to server

--- public/scripts/ngBasics.js
+++ public/scripts/ngBasics.js
@@ -1,20 +1,41 @@
 var myApp = angular.module( 'myApp', [] );
 
+// $(document).ready(getItems());
+
 //Set up controller
-myApp.controller( 'InventoryController', function(){
+myApp.controller( 'InventoryController', function($http){
   console.log('-->NG');
 
   var vm = this;
-  vm.items = [];
+  // vm.items = [];
 
   vm.addItem = function(){
     var newItem = {
       name: vm.nameIn,
       description: vm.descriptionIn
     };//end newItem
+  $http({
+    method:'POST',
+    url: '/addItem',
+    data: newItem,
+  }).then(function success(response) {
+   console.log('resp ->', response.data);
+  //  getItems();
+ });//end successs
+ getItems();
+};
 
-    vm.items.push( newItem );
-    vm.nameIn = '';
-    vm.descriptionIn = '';
-  };//end addItem  
+function getItems(){
+  $http({
+    method: 'GET',
+    url:'/getItems',
+  }).then(function success(response) {
+   console.log('resp ->', response.data);
+   vm.itemData = response.data;
+  });
+}
+
+
+vm.nameIn = '';
+vm.descriptionIn = '';
 });//end inventory controller
--- server/app.js
+++ server/app.js
@@ -9,9 +9,23 @@ var index = require('./modules/index');
 //uses
 app.use( express.static( 'public' ) );
 app.use( bodyParser.urlencoded( { exteneded: true} ) );
+app.use( bodyParser.json( ) );
+
+//globals
+var items = [];
 
 //routes
 app.use('/', index);
+
+app.get('/getItems', function( req, res ){
+  res.send(items);
+});
+
+app.post('/addItem', function( req, res ){
+  console.log(req.body);
+  items.push(req.body);
+  res.send( 200 );
+});
 
 //globals
 var port = process.env.PORT || 3456;
